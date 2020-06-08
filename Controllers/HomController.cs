991b021e74726b6e31b96007950bd95cb7b2e512
Cody Calhoun <w.calhoun@outlook.com>
2020-06-07 22:05:09 -0700

    second commit

---
+++ Controllers/HomController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio
+{
+
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+
+        public string Index()
+        {
+            return "This is my index!";
+        }
+
+        [HttpGet("projects")]
+
+        public string Projects()
+        {
+            return "These are my projects.";
+        }
+
+        [HttpGet("contact")]
+
+        public string Contacts()
+        {
+            return "This is my Contact!";
+        }
+    }
+}
